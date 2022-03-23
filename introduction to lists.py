eee78b999076904d4f689b2e088144efe29ff402
pendemmanideep <76700128+pendemmanideep@users.noreply.github.com>
2022-03-23 21:19:29 +0530

    Add files via upload

---
+++ introduction to lists.py
@@ -0,0 +1,8 @@
+list1=[1,2,True,4.5,"A","Manideep","saideep"]
+print(list1)
+print(list1[1])          #Accessing the list
+print(list1[1:4])        #slicing the lists
+print(list1[1:])         #slice start to end
+print(list1[:4])
+print(list1[::-1])         #list  in reverse order
+print(list1[-4:-1])          #using negative indexing
